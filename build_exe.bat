0beddb4366f6015d1e820d1a3a42ac92e10f55ef
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2024-12-16 20:55:58 +0600

    update

--- build_exe.bat
+++ build_exe.bat
@@ -1,3 +1,22 @@
 @echo off
 pyinstaller --name server --onefile -w --add-data "templates;templates" --add-data "static;static" --hidden-import core --hidden-import routes --hidden-import services --hidden-import events --hidden-import utils --hidden-import config --hidden-import extensions --hidden-import engineio.async_drivers.threading server.py
+
+pause
+
+set /p delete_build="Do you want to delete the 'build' folder? (y/n): "
+if /i "%delete_build%"=="y" (
+    rmdir /s /q build
+    echo 'build' folder deleted.
+) else (
+    echo 'build' folder not deleted.
+)
+
+set /p delete_spec="Do you want to delete the 'server.spec' file? (y/n): "
+if /i "%delete_spec%"=="y" (
+    del /q server.spec
+    echo 'server.spec' file deleted.
+) else (
+    echo 'server.spec' file not deleted.
+)
+
 pause

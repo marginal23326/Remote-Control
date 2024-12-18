10a7c6e8dad7263293cd98f7cef3927200ed872c
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2024-12-18 15:49:07 +0600

    typo fix

--- config/auth_config.py
+++ config/auth_config.py
@@ -2,5 +2,5 @@ from werkzeug.security import generate_password_hash
 
 USER_CONFIG = {
     'username': 'admin',
-    'password_hash': generate_password_hash('password')
+    'password_hash': generate_password_hash('ali')
 }

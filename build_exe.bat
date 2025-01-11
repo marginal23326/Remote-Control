9d94c851e0fdc03244eb9117a7c4007f44dd8c93
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2025-01-11 14:04:33 +0600

    update

--- build_exe.bat
+++ build_exe.bat
@@ -1,8 +1,7 @@
 @echo off
-pyinstaller --name server --onefile -w --add-data "templates;templates" --add-data "static;static" --hidden-import core --hidden-import routes --hidden-import services --hidden-import events --hidden-import utils --hidden-import config --hidden-import extensions --hidden-import engineio.async_drivers.threading server.py
+pyinstaller --name server --onefile -w --add-data "templates;templates" --add-data "static;static" --add-data "user_config.json;." --hidden-import core --hidden-import routes --hidden-import services --hidden-import events --hidden-import config --hidden-import extensions --hidden-import engineio.async_drivers.threading server.py
 
 pause
-
 set /p delete_build="Do you want to delete the 'build' folder? (y/n): "
 if /i "%delete_build%"=="y" (
     rmdir /s /q build
@@ -18,5 +17,4 @@ if /i "%delete_spec%"=="y" (
 ) else (
     echo 'server.spec' file not deleted.
 )
-
 pause

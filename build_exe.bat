29788d0fa27ed88dd13043cc739c9ac3d52bef84
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2025-03-23 19:37:46 +0600

    chore: update build scripts  and bump `tailwindcss` to `4.0.15`

--- build_exe.bat
+++ build_exe.bat
@@ -1,7 +1,13 @@
 @echo off
 pyinstaller --name server -w -F --clean --log-level WARN ^
     --add-data "templates;templates" ^
-    --add-data "static;static" ^
+    --add-data "static/css;static/css" ^
+    --add-data "static/js;static/js" ^
+    --add-data "static/node_modules/@xterm/xterm/css;static/node_modules/@xterm/xterm/css" ^
+    --add-data "static/node_modules/@xterm/xterm/lib;static/node_modules/@xterm/xterm/lib" ^
+    --add-data "static/node_modules/@xterm/addon-fit/lib;static/node_modules/@xterm/addon-fit/lib" ^
+    --add-data "static/node_modules/@xterm/addon-web-links/lib;static/node_modules/@xterm/addon-web-links/lib" ^
+    --add-data "static/node_modules/socket.io-client/dist;static/node_modules/socket.io-client/dist" ^
     --add-data ".\config\user_config.json;." ^
     --hidden-import engineio.async_drivers.threading ^
     --exclude-module numpy.f2py ^
--- create_zip.bat
+++ create_zip.bat
@@ -3,7 +3,24 @@ set ZIP_NAME=a.zip
 
 cd /d "%~dp0"
 
-7z a %ZIP_NAME% app.py server.py extensions.py config core routes events static templates -xr!__pycache__ -xr!.ruff_cache
+7z a %ZIP_NAME% ^
+    app.py ^
+    server.py ^
+    extensions.py ^
+    config ^
+    core ^
+    routes ^
+    events ^
+    templates ^
+    static\css ^
+    static\js ^
+    static\node_modules\socket.io-client\dist ^
+    static\node_modules\@xterm\xterm\css ^
+    static\node_modules\@xterm\xterm\lib ^
+    static\node_modules\@xterm\addon-fit\lib ^
+    static\node_modules\@xterm\addon-web-links\lib ^
+    -xr!__pycache__ ^
+    -xr!.ruff_cache
 
 echo.
 echo Archive %ZIP_NAME% has been created successfully!
